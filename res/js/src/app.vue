7dce251811f88402bc4d52f25283300f64dcecf7
jrc03c <jrc03c@pm.me>
2022-08-20 09:06:49 -0500

    ...

---
+++ res/js/src/app.vue
@@ -0,0 +1,13 @@
+<style scoped>
+  .app {
+    color: purple;
+  }
+</style>
+
+<template>
+  <div class="app">Hello, world!</div>
+</template>
+
+<script setup>
+  console.log("Hello from the console!")
+</script>
---
+++ res/js/src/main.js
@@ -0,0 +1,3 @@
+import { createApp } from "vue"
+import app from "./app.vue"
+createApp(app).mount("#app")
